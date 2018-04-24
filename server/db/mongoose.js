255ef4f7c8b4d8131bc829e446c68424ed5c1c19
Khojiakbar Karimov <kh.karimov96@gmail.com>
2018-04-24 09:45:27 -0700

    mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose=require('mongoose')
 
 mongoose.Promise=global.Promise
 
-mongoose.connect('mongodb://mredem96:lenovoideapad100@ds253959.mlab.com:53959/todo-api||mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://mredem96:lenovoideapad100@ds253959.mlab.com:53959/todo-api')
 
 module.exports={mongoose} 
