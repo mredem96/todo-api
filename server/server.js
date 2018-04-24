b773415864ecee9873bb1514a36354fbb9a8c606
Khojiakbar Karimov <kh.karimov96@gmail.com>
2018-04-24 02:11:05 -0700

    Add GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express=require('express')
 var bodyParser=require('body-parser')
-
+const {ObjectID}=require('mongodb')
 var {mongoose}=require('./db/mongoose')
 var {Todo}=require('./modules/todo')
 var {User}=require('./modules/user')
@@ -20,6 +20,31 @@ app.post('/todos',(req,res)=>{
     },(e)=>{
         console.log('unable to post')
         res.status(400).send(e)
+    })
+})
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send(todos)
+    },(err)=>{
+        res.status(400).send(err)
+    })
+})
+
+app.get('todos/:id',(req,res)=>{
+    var id =req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(todo){
+            res.send(todo)
+        }
+        res.status(404).send()
+    },(e)=>{
+        res.status(400).send()
     })
 })
 
